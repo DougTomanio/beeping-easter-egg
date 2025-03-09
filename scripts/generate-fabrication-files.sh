dba711a186e73a034d822377c987ac4c93d7719a
Doug Tomanio <2694864+DougTomanio@users.noreply.github.com>
2025-03-09 18:37:06 -0400

    Use jlcpcb spec for tooling holes. Changed panelization frame type to tightframe.

--- scripts/generate-fabrication-files.sh
+++ scripts/generate-fabrication-files.sh
@@ -10,13 +10,12 @@ fi
 
 kikit panelize \
     --layout "hspace: 2mm; vspace: 2mm; rows: ${ROWS=2}; cols: ${COLS=2}" \
-    --tabs "type: fixed" \
+    --tabs 'type: corner; width: 5mm' \
     --cuts "type: mousebites" \
-    --framing "type: frame; width: 6mm" \
-    --tooling "type: 4hole; hoffset: 14mm; voffset: 3mm" \
+    --framing 'type: tightframe' \
+    --tooling 'type: 4hole; size: 1.152mm; soldermaskmargin: 0.148mm; hoffset: 5mm; voffset: 2.5mm' \
     --fiducials "type: 3fid; hoffset: 20mm; voffset: 3.85mm; coppersize: 2mm" \
-    --text "type: simple; anchor: ml; orientation: 90deg; hoffset: 3mm; text: JLCJLCJLCJLC" \
+    --text "type: simple; anchor: ml; orientation: 90deg; hoffset: 2.5mm; text: JLCJLCJLCJLC" \
     "/app/beeping-easter-egg.kicad_pcb" "/app/beeping-easter-egg-panelized.kicad_pcb"
 
-kikit fab jlcpcb /app/beeping-easter-egg.kicad_pcb /app/fab --assembly --schematic /app/beeping-easter-egg.kicad_sch
-kikit fab jlcpcb /app/beeping-easter-egg-panelized.kicad_pcb /app/fab-panelized --assembly --schematic /app/beeping-easter-egg.kicad_sch
+kikit fab jlcpcb /app/beeping-easter-egg-panelized.kicad_pcb /app/fab --assembly --schematic /app/beeping-easter-egg.kicad_sch
