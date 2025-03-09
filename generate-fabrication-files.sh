1633444241c347b819fba85d012e9d992880f723
Doug Tomanio <2694864+DougTomanio@users.noreply.github.com>
2025-03-08 20:44:12 -0500

    Make sure we are running in the kicad docker container before doing kikit setup

--- generate-fabrication-files.sh
+++ generate-fabrication-files.sh
@@ -1,10 +1,15 @@
 # todo: use the official kikit docker image once it supports kikad 9, or at least roll own proper docker image for this
-sudo apt update
-sudo apt install python3-pip -y
-pip install --break-system-packages kikit
 
-/home/kicad/.local/bin/kikit panelize \
-    --layout 'hspace: 2mm; vspace: 2mm; rows: 3; cols: 3' \
+# quick check to make sure we're running in the kicad docker container
+if [ "$HOME" == "/home/kicad" ]; then
+    sudo apt update
+    sudo apt install python3-pip -y
+    pip install --break-system-packages kikit
+    PATH=$PATH:/home/kicad/.local/bin
+fi
+
+kikit panelize \
+    --layout 'hspace: 2mm; vspace: 2mm; rows: 2; cols: 2' \
     --tabs 'type: fixed' \
     --cuts 'type: mousebites' \
     --framing 'type: railstb' \
@@ -13,5 +18,5 @@ pip install --break-system-packages kikit
     --text 'type: simple; voffset: 2.5mm; text: JLCJLCJLCJLC' \
     '/app/beeping-easter-egg.kicad_pcb' '/app/beeping-easter-egg-panelized.kicad_pcb'
 
-/home/kicad/.local/bin/kikit fab jlcpcb /app/beeping-easter-egg.kicad_pcb /app/fab --assembly --schematic /app/beeping-easter-egg.kicad_sch
-/home/kicad/.local/bin/kikit fab jlcpcb /app/beeping-easter-egg-panelized.kicad_pcb /app/fab-panelized --assembly --schematic /app/beeping-easter-egg.kicad_sch
+kikit fab jlcpcb /app/beeping-easter-egg.kicad_pcb /app/fab --assembly --schematic /app/beeping-easter-egg.kicad_sch
+kikit fab jlcpcb /app/beeping-easter-egg-panelized.kicad_pcb /app/fab-panelized --assembly --schematic /app/beeping-easter-egg.kicad_sch
